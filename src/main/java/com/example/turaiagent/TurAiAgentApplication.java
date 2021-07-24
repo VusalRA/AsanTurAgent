9867866fba86e78a02a841efbf58d48bf6872565
Vusal Abdullayev <Theabdullayev6>
2021-07-24 09:12:38 +0400

    Initial commit

---
+++ src/main/java/com/example/turaiagent/TurAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.turaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/turaiagent/TurAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
